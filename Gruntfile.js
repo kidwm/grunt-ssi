6a93e664eadb245cf1f8481890ebf316b3752b81
Angus Perkerson <angusp@angup.com>
2014-03-03 11:55:05 -0800

    Adds Better Grunt Logging for verbose and non verbose Also fixes the issue of the cache option not being properly passed to the ssi task

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,83 +8,95 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp', '.tmp'],
-    },
-
-    // Configuration to be run (and then tested).
-    ssi: {
-      options: {},
-      default_options: {
-        options: {},
-        files: [{
-          expand: true,
-          src: ['html/*'],
-        }],
-      },
-      custom_options: {
-        options: {
-          cacheDir: '',
-
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>',
+            ],
+            options: {
+                jshintrc: '.jshintrc',
+            },
         },
-        files: [{
-          expand: true,
-          cwd: 'html',
-          src: ['**/*.html'],
-          dest: '.tmp/html',
-          ext: '.html',
-        }],
-      },
-      test_default: {
-        options: {
-          baseDir: 'test/fixtures/html/',
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp', '.tmp'],
         },
-        files: [{
-          expand: true,
-          cwd: 'test/fixtures',
-          src: ['html/**/*.html'],
-          dest: '.tmp/',
-          ext: '.html',
-        }],
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        // Configuration to be run (and then tested).
+        ssi: {
+            options: {},
+            default_options: {
+                options: {},
+                files: [{
+                    expand: true,
+                    src: ['html/*'],
+                }],
+            },
+            custom_options: {
+                options: {
+                    cacheDir: '',
 
-  });
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'html',
+                    src: ['**/*.html'],
+                    dest: '.tmp/html',
+                    ext: '.html',
+                }],
+            },
+            test_default: {
+                options: {
+                    baseDir: 'test/fixtures/html/',
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'test/fixtures',
+                    src: ['html/**/*.html'],
+                    dest: '.tmp/',
+                    ext: '.html',
+                }],
+            },
+            cache_options: {
+                options: {
+                    cache: true,
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'html',
+                    src: ['**/*.html'],
+                    dest: '.tmp/',
+                    ext: '.html',
+                }],
+            }
+        },
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
+        },
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    });
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'ssi:test_default', 'nodeunit']);
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'ssi:test_default', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/ssi.js
+++ tasks/ssi.js
@@ -8,64 +8,64 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  var SSI = require('./lib/ssi.js')(grunt.log.writeln);
-  var path = require('path');
+    var SSI = require('./lib/ssi.js')(grunt.log.writeln, grunt);
+    var path = require('path');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('ssi', 'Compiles HTML with SSI into static HTML pages', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var defaults = {
-      cacheDir: '.tmp/.ssiCache',
-      fileSep: path.sep,
-      includesRegex: /<!--\#include\s+(file|virtual)=["']([^"'<>|\b]+)['"]\s+-->/g,
-      cache: false,
-      ext: '.html',
-      encoding: 'utf8',
-      baseDir: 'html',
-    };
+    grunt.registerMultiTask('ssi', 'Compiles HTML with SSI into static HTML pages', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var defaults = {
+            cacheDir: '.tmp/.ssiCache',
+            fileSep: path.sep,
+            includesRegex: /<!--\#include\s+(file|virtual)=["']([^"'<>|\b]+)['"]\s+-->/g,
+            cache: false,
+            ext: '.html',
+            encoding: 'utf8',
+            baseDir: 'html',
+        };
 
-    var options = this.options(defaults);
+        var options = this.options(defaults);
 
-    var ssi = new SSI(options);
+        var ssi = new SSI(options);
 
-    if (!grunt.file.exists(options.cacheDir)) {
-      grunt.file.mkdir(options.cacheDir);
-    }
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-
-      grunt.log.writeln('BaseDir: ' + options.baseDir);
-      ssi.setBaseDir(options.baseDir);
-
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath) || grunt.file.isDir(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        if (!grunt.file.exists(options.cacheDir)) {
+            grunt.file.mkdir(options.cacheDir);
         }
-      }).forEach(function(filepath) {
 
-        grunt.log.writeln('File to process: ' + f.src + ' - ' + filepath + ' dest: ' + f.dest);
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
 
-        var data = ssi.processFile(filepath, options.cache);
+            grunt.verbose.writeln('BaseDir: ' + options.baseDir);
+            ssi.setBaseDir(options.baseDir);
 
-        var dest = f.dest ? f.dest : path.join('output', filepath); // + path.sep + path.basename(filepath, path.extname(filepath)) + options.ext;
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath) || grunt.file.isDir(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).forEach(function (filepath) {
 
-        grunt.file.write(dest, data);
-        grunt.log.writeln('File "' + dest + '" created.');
+                grunt.verbose.or.write('Creating file \'' + f.dest + '\'... ');
+                grunt.verbose.write('Creating File \'' + f.dest + '\' from \'' + filepath + '\'')
+                var data = ssi.processFile(filepath, null, options.cache);
 
-      });
+                var dest = f.dest ? f.dest : path.join('output', filepath); // + path.sep + path.basename(filepath, path.extname(filepath)) + options.ext;
+
+                grunt.file.write(dest, data);
+                grunt.verbose.or.ok();
+                grunt.verbose.ok();
+            });
+
+        });
 
     });
-
-  });
 
 };
